7eb666e602c29dee9b0857896ea6b18865c6a4a3
Brian Scoles <brianscoles@gmail.com>
2014-09-21 17:24:07 -0700

    wrote out skeleton of solution, still contains some pseudocode.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,8 +2,18 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
+// You should use document.body, element.childNodes, and element.classList
+// what you want to return is an "HTMLcollection."
+// https://dvcs.w3.org/hg/domcore/raw-file/tip/Overview.html#dom-document-getelementsbyclassname
+// learn up on how HTML nodes work... how am I supposed to iterate through the whole document? and it will require recursion (I assume.)
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var classes = classname.split(' ');  // make an array.
+  var result = [];
+
+  //iterate through every element on the page.
+  // if element.classList contains ALL elements listed in "classes"
+  // then append that element to "result."
 };
