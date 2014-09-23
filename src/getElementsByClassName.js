3431e698873784124021ab690b59db223447dc82
Brian Scoles <brianscoles@gmail.com>
2014-09-23 08:42:58 -0700

    getElementsByClassName now working! not sure what i changed on stringifyJSON.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,40 +1,36 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-// You should use document.body, element.childNodes, and element.classList
-// what you want to return is an "HTMLcollection."
-// https://dvcs.w3.org/hg/domcore/raw-file/tip/Overview.html#dom-document-getelementsbyclassname
-// learn up on how HTML nodes work... how am I supposed to iterate through the whole document? and it will require recursion (I assume.)
+// duplicates functionality of built-in JS function Node.getElementsByClassName()
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
-  var targetClasses = classname.split(' ');  // make an array.
-  var result = [];
+function getElementsByClassName(className){
+  
+    var targetClasses = className.split(' ');  // make an array.
+    var result = [];
 
-function containsAll(arr1,arr2){
-	for (var i = 0; i < arr1.length; i++) {
-		if(arr2.indexOf(arr1[i])===-1) return false;
-	}
-	return true;
-}
+    function containsAll(arr1,arr2){
+      for (var i = 0; i < arr1.length; i++) {
+        if(arr2.indexOf(arr1[i])===-1) return false;
+      }
+      return true;
+    }
 
-function permute(element){
-  var elemClasses = element.classList();
-  if(containsAll(targetClasses,elemClasses){
-  	result.push(element);
+    function permute(element){
+      cycles = cycles + 1;
+      var elemClasses = element.classList;
+      if(typeof(elemClasses)!=='undefined'){
+        elemClasses = [].slice.call(elemClasses);
+        if(containsAll(targetClasses,elemClasses)){
+          result.push(element);
+        }
+      }
+      if(element.hasChildNodes()){
+        var nodes = [].slice.call(element.childNodes);
+        for (var i = 0; i < nodes.length; i++) {
+          permute(nodes[i]);
+        }
+      }
+    };
+
+    permute(document.body);
+    return result;
   }
-  var nodes = element.childNodes();
-  for (var i = 0; i < nodes.length; i++) {
-  	permute(nodes[i]);
-  }
- };
- 
-  permute(document);
 
-}
-  //iterate through every element on the page.
-  // if element.classList contains ALL elements listed in "classes"
-  // then append that element to "result."
-};
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,39 +1,3 @@
-/*
-
-JSON.stringify({});                        // '{}'
-JSON.stringify(true);                      // 'true'
-JSON.stringify("foo");                     // '"foo"'
-JSON.stringify([1, "false", false]);       // '[1,"false",false]'
-JSON.stringify({ x: 5 });                  // '{"x":5}'
-
-JSON.stringify({x: 5, y: 6});              
-// '{"x":5,"y":6}' or '{"y":6,"x":5}'
-JSON.stringify([new Number(1), new String("false"), new Boolean(false)]); 
-// '[1,"false",false]'
-
-// Symbols:
-JSON.stringify({x: undefined, y: Object, z: Symbol("")}); 
-// '{}'
-JSON.stringify({[Symbol("foo")]: "foo"});                 
-// '{}'
-JSON.stringify({[Symbol.for("foo")]: "foo"}, [Symbol.for("foo")]);
-// '{}'
-JSON.stringify({[Symbol.for("foo")]: "foo"}, function (k, v) {
-  if (typeof k === "symbol"){
-    return "a symbol";
-  }
-});
-// '{}'
-
-*/
-
-var sampleArr = ["pooper", 100, 22, "yellow", true, [57, "pinky"], "true"];
-var sampleObj = {a: {b: {c: 5}}};
-var sampleObj2 = {"foo":true,"bar":false,"baz":null}
-var sampleObj3 = {"functions":function (){},"undefined":undefined}
-var sampleObj4 = {"a": "apple"}
-
-
 var stringifyJSON = function(obj) {
 
   function permute(elem){
